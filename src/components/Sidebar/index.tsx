52bd6ac8b476daa7bdc804fe2131522ea540c40c
Gabriel <gspadovesi@gmail.com>
2022-12-12 16:40:59 -0300

    Criando sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,42 @@
+import { Container } from "./styles";
+import { ReactComponent as HairIcon } from "../../assets/barbearia.svg";
+import { ReactComponent as BarbaIcon } from "../../assets/barba.svg";
+import { ReactComponent as SobrancelhaIcon } from "../../assets/sobrancelha.svg";
+import { ReactComponent as TelIcon } from "../../assets/telefone.svg";
+import logo from "../../assets/menu.svg";
+
 export function Sidebar(){
-    return <h1>Sidebar</h1>
+    return <Container>
+            <button type="button">
+                <img src={logo} alt="Abrir e fechar sidebar" />
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#" className="active">
+                            <HairIcon />
+                            <span>Cabelo</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <BarbaIcon />
+                            <span>Barba</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <SobrancelhaIcon />
+                            <span>Barba</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <TelIcon />
+                            <span>Contatos</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </Container>
 }
