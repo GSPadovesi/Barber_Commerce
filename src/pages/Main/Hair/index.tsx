54e7808e05cde3ce9292546d282982f3a593d7cf
Gabriel <gspadovesi@gmail.com>
2022-12-15 15:57:58 -0300

    Preparando .map

--- src/pages/Main/Hair/index.tsx
+++ src/pages/Main/Hair/index.tsx
@@ -1,11 +1,31 @@
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
+import { Snacks } from "../../../components/Snacks"
 
 export default function Hair(){
+
+    const data = [
+        {
+            id:'1',
+            name:'Corte Disfarçado Longo',
+            price:'R$ 150,00',
+            description:'Corte de cabelo feminino',
+            image: 'https://i.imgur.com/fmvhB8r.jpeg'
+        },
+        {
+            id:'2',
+            name:'Corte Disfarçado Curto',
+            price:'R$ 50,00',
+            description:'Corte de cabelo masculino',
+            image: 'https://i.imgur.com/RTRsXFV.jpeg'
+        }
+    ]
+    
     return(
         <>
             <Head title="Cabelo" description="Página de Cabelo" />
             <SnackTitle>Cabelos</SnackTitle>
+            <Snacks snacks={data} />
         </>
     )
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -6,27 +6,25 @@ export const Container = styled.main`
   display: flex;
 
   > section {
-      flex: 1;
-      width: 100%;
-      height: 100vh;
-      overflow-y: auto;
-      padding: 2rem 1.875rem;
+    flex: 1;
+    width: 100%;
+    height: 100vh;
+    overflow-y: auto;
+    padding: 2rem 1.875rem;
 
-      img{
-        width: 10rem;
-        height: 2rem;
+    img {
+      width: 10rem;
+      margin-bottom: 2rem;
+    }
+
+    @media (max-width: 720px) {
+      display: flex;
+      flex-direction: column;
+      padding-bottom: 8rem;
+
+      img {
+        align-self: center;
       }
-
-      @media (max-width: 720px) {
-        display: flex;
-        flex-direction: column;
-        padding-bottom: 8rem;
-
-        img{
-          align-self: center;
-          color: #fff;
-        }
-      }
+    }
   }
-  
 `
